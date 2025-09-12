82cd2e116bf723edde62f68712640033b9ffe99e
Armin <armin2003sheibak@gmail.com>
2025-09-12 17:54:06 +0330

    add env variables

--- packages/server/index.ts
+++ packages/server/index.ts
@@ -1,5 +1,7 @@
 import express from "express";
 import type { Request, Response } from "express";
+import dotenv from 'dotenv'
+dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 3000;
