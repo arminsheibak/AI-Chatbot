cf1019058b8885897102a8eec31383ec63c8df37
Armin <armin2003sheibak@gmail.com>
2025-09-29 18:52:45 +0330

    add cors

--- packages/server/index.ts
+++ packages/server/index.ts
@@ -1,12 +1,21 @@
 import express from "express";
 import dotenv from "dotenv";
 import router from "./routes";
+import cors from 'cors'
 
 dotenv.config();
 
+const corsOptions = {
+  origin: "*",
+  methods: ["GET", "POST", "PATCH", "DELETE", "PUT"],
+};
+
 const app = express();
+app.use(cors(corsOptions))
 app.use(express.json());
 app.use(router);
+
+
 
 const port = process.env.PORT || 3000;
 
